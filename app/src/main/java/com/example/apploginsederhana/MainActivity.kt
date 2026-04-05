48474e4fb86a2dfc7ddb7fa342f0c70b4c86d16a
fadilahsan <140679844+xeesanxe@users.noreply.github.com>
2026-04-05 22:13:18 +0700

    Login Sederhana

---
+++ app/src/main/java/com/example/apploginsederhana/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.apploginsederhana
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+// Inisialisasi komponen
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+// Aksi saat tombol login diklik
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+// Validasi sederhana
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Username dan Password tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+            } else {
+// Jika sukses, akan menampilkan pesan sukses
+                Toast.makeText(this, "Anda berhasil login!",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
